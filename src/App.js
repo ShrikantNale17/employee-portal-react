499932bbe08baf4f1e8dfe886bc22c2e2b942003
ShrikantNale-angularminds <shrikant.nale@angularminds.in>
2022-08-12 16:01:01 +0530

    update

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
     <Box component={Paper} p={5} color='ThreeDShadow' bgcolor={'aliceblue'} minHeight={'100%'}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Navigate to={'/employees'} />}></Route>
-          <Route path='/employees' element={<ListOfEmployees />}></Route>
-          <Route path='/employees/add' element={<AddEmployee />}></Route>
-          <Route path='/employees/update/:id' element={<UpdateEmployee />}></Route>
+          <Route path='/' element={<Navigate to={'/employee-portal/employees'} />}></Route>
+          <Route path='/employee-portal/employees' element={<ListOfEmployees />}></Route>
+          <Route path='/employee-portal/employees/add' element={<AddEmployee />}></Route>
+          <Route path='/employee-portal/employees/update/:id' element={<UpdateEmployee />}></Route>
           <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
